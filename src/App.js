caa9a685b9d2dd0371350621bfa6aabf796bcb69
tarun1928 <tarunkonagalla2806@gmail.com>
2025-04-23 10:17:01 -0500

    remved hashrouter

--- src/App.js
+++ src/App.js
@@ -7,11 +7,12 @@ import Projects from "./components/Projects/Projects";
 import Footer from "./components/Footer";
 import Resume from "./components/Resume/ResumeNew";
 import {
-  HashRouter as Router,
+  BrowserRouter as Router,
   Route,
   Routes,
   Navigate
 } from "react-router-dom";
+
 
 import ScrollToTop from "./components/ScrollToTop";
 import "./style.css";
